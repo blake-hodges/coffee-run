c9cab05ae5b53bd5a72f36deafeb4d487960bbe7
blake-hodges <tbhodges@gmail.com>
2022-02-03 17:57:02 +0800

    add Datastore function to scripts

--- scripts/datastore.js
+++ scripts/datastore.js
@@ -4,6 +4,10 @@
 
     function DataStore() {
         console.log('running the DataStore function');
+        this.data = {};
+    }
+    DataStore.prototype.add = function (key, val) {
+        this.data[key] = val;
     }
 
     App.DataStore = DataStore;
