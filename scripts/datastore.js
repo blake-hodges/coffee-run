b98196673c30dee1837a3d506dadd532d329bb23
blake-hodges <tbhodges@gmail.com>
2022-02-05 19:15:55 +0800

    add remove method to DataStore constructor

--- scripts/datastore.js
+++ scripts/datastore.js
@@ -18,6 +18,10 @@
         return this.data;
     }
 
+    DataStore.prototype.remove = function (key) {
+        delete this.data[key];
+    }
+
 
     App.DataStore = DataStore;
     window.App = App;
