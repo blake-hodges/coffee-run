38a282bdbb815cfdb9c4a4a41bbbedccca22b265
blake-hodges <tbhodges@gmail.com>
2022-02-07 20:00:04 +0800

    add deliverOrder method to Truck constructor

--- scripts/truck.js
+++ scripts/truck.js
@@ -11,6 +11,12 @@
         this.db.add(order.emailAddress, order);
     }
 
+    Truck.prototype.deliverOrder = function (customerId) {
+        console.log('Delivering order for ' + customerId);
+        //customerId should be same as emailAddress submitted when order was created
+        this.db.remove(customerId);
+    }
+
     App.Truck = Truck;
     window.App = App;
 
