f9a954844d0f90580eb56070c8271c4e9f443c84
blake-hodges <tbhodges@gmail.com>
2022-02-16 12:47:20 +0800

    pending order info is displayed after form is submitted

--- scripts/checklist.js
+++ scripts/checklist.js
@@ -13,6 +13,41 @@
         }
     }
 
+    CheckList.prototype.addRow = function (coffeeOrder) {
+        //create a new instance of a row, using the coffee order info
+        var rowElement = new Row(coffeeOrder);
+        //add the new row instance's $element property to the checklist
+        this.$element.append(rowElement.$element);
+    }
+
+    function Row(coffeeOrder) {
+        var $div = $('<div></div>', {
+            'data-coffee-order': 'checkbox',
+            'class': 'text-red'
+        });
+
+        var $label = $('<label></label>');
+
+        var $checkbox = $('<input></input>', {
+            type: 'checkbox',
+            value: coffeeOrder.emailAddress
+        });
+
+        var description;
+        if (coffeeOrder.flavor) {
+            description = `${coffeeOrder.size} ${coffeeOrder.flavor} ${coffeeOrder.coffee}, (${coffeeOrder.emailAddress}) [${coffeeOrder.strength}]`;
+        } else {
+            description = `${coffeeOrder.size} ${coffeeOrder.coffee}, (${coffeeOrder.emailAddress}) [${coffeeOrder.strength}]`;
+        }
+
+        $label.append($checkbox);
+        $label.append(description);
+        $div.append($label);
+
+        this.$element = $div;
+    }
+
+
     App.CheckList = CheckList;
     window.App = App;
 })(window);
